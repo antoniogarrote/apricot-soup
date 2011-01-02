f10896945a0affc76362a482a9d2f42379b5ea16
Miki Tebeka <miki.tebeka@gmail.com>
2011-01-02 09:04:57 -0800

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject apricot-soup "0.0.2-SNAPSHOT"
   :description "HTML parsing, querying and manipulation"
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]
-                 [org.jsoup/jsoup "1.2.2"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.jsoup/jsoup "1.4.1"]]
   :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
